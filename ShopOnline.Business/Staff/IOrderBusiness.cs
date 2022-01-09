e3e3b3492e29d146ad441b10f87955d73440895f
TrucNguyen <ngoctruc020100@gmail.com>
2022-01-09 11:49:47 +0700

    Add list accepted shipper and add .ToLower() Search

--- ShopOnline.Business/Staff/IOrderBusiness.cs
+++ ShopOnline.Business/Staff/IOrderBusiness.cs
@@ -8,8 +8,9 @@ namespace ShopOnline.Business.Order
 {
     public interface IOrderBusiness
     {
-        Task<IPagedList<OrderInfor>> GetListOrderAsync(string sortOrder, string currentFilter, string searchString, int? page);
-        Task<IPagedList<HistoryOrderInfor>> GetHistoryOrderCustomerAsync(string sortOrder, string currentFilter, string searchString, int? page, ClaimsPrincipal user);
-        Task<IPagedList<HistoryOrderShipperInfor>> GetHistoryOrderShipperAsync(string sortOrder, string currentFilter, string searchString, int? page, ClaimsPrincipal user);
+        Task<IPagedList<OrderInfor>> GetListOrderAsync(string sortOrder, string currentFilter, int? page);
+        Task<IPagedList<HistoryOrderInfor>> GetHistoryOrderCustomerAsync(string sortOrder, string currentFilter, int? page, ClaimsPrincipal user);
+        Task<IPagedList<HistoryOrderShipperInfor>> GetHistoryOrderShipperAsync(string sortOrder, string currentFilter, int? page, ClaimsPrincipal user);
+        Task<IPagedList<OrderInforShipper>> GetOrderShipperAsync(string sortOrder, string currentFilter, int? page, ClaimsPrincipal user);
     }
 }
--- ShopOnline/Controllers/Staff/OrderController.cs
+++ ShopOnline/Controllers/Staff/OrderController.cs
@@ -22,40 +22,46 @@ namespace ShopOnline.Controllers.Staff
         }
 
         [Authorize(Roles = ROLE.STAFF)]
-        public async Task<IActionResult> ListOrder(string sortOrder, string currentFilter, string searchString, int? page)
+        public async Task<IActionResult> ListOrder(string sortOrder, string currentFilter, int? page)
         {
             ViewBag.CurrentSort = sortOrder;
             ViewBag.OrderDay = String.IsNullOrEmpty(sortOrder) ? "order_day_desc" : "";
 
-            if (searchString != null) page = 1;
-            else searchString = currentFilter;
-            ViewBag.CurrentFilter = searchString;
-
             var model = new OrderModel
             {
-                ListOrder = await _orderBusiness.GetListOrderAsync(sortOrder, currentFilter, searchString, page)
+                ListCustomer = await _reviewBusiness.GetListCustomer(),
+                ListOrder = await _orderBusiness.GetListOrderAsync(sortOrder, currentFilter, page)
             };
 
             return View(model);
         }
 
-        public async Task<IActionResult> ListHistoryOrderShipperAsync(string sortOrder, string currentFilter, string searchString, int? page)
+        public async Task<IActionResult> ListHistoryOrderShipperAsync(string sortOrder, string currentFilter, int? page)
         {
             ViewBag.CurrentSort = sortOrder;
             ViewBag.OrderDay = String.IsNullOrEmpty(sortOrder) ? "order_day" : "";
 
-            if (searchString != null) page = 1;
-            else searchString = currentFilter;
-            ViewBag.CurrentFilter = searchString;
-
             var model = new HistoryOrderShipperModel
             {
                 ListCustomer = await _reviewBusiness.GetListCustomer(),
-                ListHistoryOrderShipper = await _orderBusiness.GetHistoryOrderShipperAsync(sortOrder, currentFilter, searchString, page, User)
+                ListHistoryOrderShipper = await _orderBusiness.GetHistoryOrderShipperAsync(sortOrder, currentFilter, page, User)
             };
 
             return View(model);
         }
 
+        public async Task<IActionResult> ListOrderShipperAsync(string sortOrder, string currentFilter, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.OrderDay = String.IsNullOrEmpty(sortOrder) ? "order_day" : "";
+
+            var model = new OrderInforShipperModel
+            {
+                ListCustomer = await _reviewBusiness.GetListCustomer(),
+                ListOrderInforShipper = await _orderBusiness.GetOrderShipperAsync(sortOrder, currentFilter, page, User)
+            };
+
+            return View(model);
+        }
     }
 }
