c88761fab32fa2caddd6689e2b74886095a1317e
TrucNguyen <ngoctruc020100@gmail.com>
2022-01-09 20:38:54 +0700

    Apply Repository Pattern To Product Entity

---
+++ ShopOnline.Data/Repositories/Product/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopOnline.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Data.Repositories.Product
+{
+    public interface IProductRepository : IRepositoryBase<ProductEntity>
+    {
+    }
+}
---
+++ ShopOnline.Data/Repositories/Product/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using ShopOnline.Core;
+using ShopOnline.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Data.Repositories.Product
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MyDbContext _context;
+
+        public ProductRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<ProductEntity> Get()
+        {
+            return _context.Products.Where(x => !x.IsDeleted);
+        }
+
+        public void Add(ProductEntity productEntity)
+        {
+            _context.Products.Add(productEntity);
+        }
+
+        public void Update(ProductEntity productEntity)
+        {
+            _context.Products.Update(productEntity);
+        }
+
+        public void SoftDelete(ProductEntity productEntity)
+        {
+            productEntity.IsDeleted = true;
+            _context.Products.Update(productEntity);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
